1dd080e56569304f499e5e439693870838d09f06
JBuschkind <buschjohann@gmail.com>
2020-12-16 20:55:12 +0100

    Update WerwolfServer.java

--- WerwolfServer.java
+++ WerwolfServer.java
@@ -49,6 +49,81 @@ public class WerwolfServer extends WebSocketServer {
 		int port = 3001;
 		
 		WebSocketServer server = new WerwolfServer(new InetSocketAddress(host, port));
+		
+		SSLContext context = getContext();
+		if (context != null) {
+			server.setWebSocketFactory(new DefaultSSLWebSocketServerFactory(getContext()));
+		}
 		server.run();
 	}
+	
+	private static SSLContext getContext() {
+    SSLContext context;
+    String password = "thisisapassword";
+    String pathname = "/etc/letsencrypt/live/busch.click";
+    try {
+      context = SSLContext.getInstance("TLS");
+
+      byte[] certBytes = parseDERFromPEM(getBytes(new File(pathname + File.separator + "fullchain.pem")),
+          "-----BEGIN CERTIFICATE-----", "-----END CERTIFICATE-----");
+      byte[] keyBytes = parseDERFromPEM(
+          getBytes(new File(pathname + File.separator + "privkey.pem")),
+          "-----BEGIN PRIVATE KEY-----", "-----END PRIVATE KEY-----");
+
+      X509Certificate cert = generateCertificateFromDER(certBytes);
+      RSAPrivateKey key = generatePrivateKeyFromDER(keyBytes);
+
+      KeyStore keystore = KeyStore.getInstance("JKS");
+      keystore.load(null);
+      keystore.setCertificateEntry("cert-alias", cert);
+      keystore.setKeyEntry("key-alias", key, password.toCharArray(), new Certificate[]{cert});
+
+      KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+      kmf.init(keystore, password.toCharArray());
+
+      KeyManager[] km = kmf.getKeyManagers();
+
+      context.init(km, null, null);
+    } catch (Exception e) {
+      context = null;
+    }
+    return context;
+  }
+
+  private static byte[] parseDERFromPEM(byte[] pem, String beginDelimiter, String endDelimiter) {
+    String data = new String(pem);
+    String[] tokens = data.split(beginDelimiter);
+    tokens = tokens[1].split(endDelimiter);
+    return DatatypeConverter.parseBase64Binary(tokens[0]);
+  }
+
+  private static RSAPrivateKey generatePrivateKeyFromDER(byte[] keyBytes)
+      throws InvalidKeySpecException, NoSuchAlgorithmException {
+    PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+
+    KeyFactory factory = KeyFactory.getInstance("RSA");
+
+    return (RSAPrivateKey) factory.generatePrivate(spec);
+  }
+
+  private static X509Certificate generateCertificateFromDER(byte[] certBytes)
+      throws CertificateException {
+    CertificateFactory factory = CertificateFactory.getInstance("X.509");
+
+    return (X509Certificate) factory.generateCertificate(new ByteArrayInputStream(certBytes));
+  }
+
+  private static byte[] getBytes(File file) {
+    byte[] bytesArray = new byte[(int) file.length()];
+
+    FileInputStream fis = null;
+    try {
+      fis = new FileInputStream(file);
+      fis.read(bytesArray); //read file into bytes[]
+      fis.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return bytesArray;
+  }
 }
