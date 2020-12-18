2f5e751e9bceadf18370b458a79ef33129b37502
JBuschkind <buschjohann@gmail.com>
2020-12-18 21:04:08 +0100

    Update WerwolfServer.java

--- WerwolfServer.java
+++ WerwolfServer.java
@@ -45,8 +45,11 @@ public class WerwolfServer extends WebSocketServer {
 	@Override
 	public void onOpen(WebSocket conn, ClientHandshake handshake) {
 		conn.send("Lets play some Werwolf!"); //This method sends a message to the new client
+		System.out.println("Hier1");
 		connections.add(conn);	//Adds connection to List of all connections
+		System.out.println("Hier2");
 		names.put(conn,getRandomName()); //Gives the Player a random Name
+		System.out.println("Hier3");
 		System.out.println(names.get(conn));
 		broadcast( "[addPlayer]:"+names.get(conn)  ); //This method sends a message to all clients connected
 		System.out.println("new connection to " + conn.getRemoteSocketAddress() + "with the name" + names.get(conn));
