1720bc1dfab247efffb9d329610e35e55f3bf9ce
JBuschkind <buschjohann@gmail.com>
2020-12-18 23:35:32 +0100

    Update WerwolfServer.java

--- WerwolfServer.java
+++ WerwolfServer.java
@@ -30,18 +30,18 @@ import org.java_websocket.server.DefaultSSLWebSocketServerFactory;
 
 public class WerwolfServer extends WebSocketServer {
 
-	public static String phase;
-	public HashMap<String,LinkedList<WebSocket>> Rollen;
+	public static String phase = "";
+	public HashMap<String,LinkedList<WebSocket>> Rollen = new HashMap<>();
 	public static LinkedList<WebSocket> connections = new LinkedList<>();
-	public HashMap<WebSocket,String> names;
-	public String defaultNames[];
+	public HashMap<WebSocket,String> names = new HashMap<>();
+	public String defaultNames[] = new String[]{"Anna","Bob","Manfred","Fritz","TinaToastbrot","Alice","MaxMustermann","Pascal"};
 	
 	public WerwolfServer(){
-		phase = "";
-		Rollen = new HashMap<>();
+		//phase = "";
+		//Rollen = new HashMap<>();
 		//connections = new LinkedList<>();
-		names = new HashMap<>();
-		defaultNames = new String[]{"Anna","Bob","Manfred","Fritz","TinaToastbrot","Alice","MaxMustermann","Pascal"};
+		//names = new HashMap<>();
+		//defaultNames = new String[]{"Anna","Bob","Manfred","Fritz","TinaToastbrot","Alice","MaxMustermann","Pascal"};
 	}
 	
   
