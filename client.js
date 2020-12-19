5cf28bd9d7b5a7bd0e1c52e9d4484ecc9f3afd3f
JBuschkind <buschjohann@gmail.com>
2020-12-19 01:41:06 +0100

    dh

--- client.js
+++ client.js
@@ -1,2 +1,17 @@
 	const socket = new WebSocket("wss://busch.click:3001");
+	
+	socket.onmessage = function(event) {
+		console.debug("WebSocket message received:", event);
+		const befehle = event.split(";");
+		for each (a in befehle){
+			const befehle2 = a.split(":")
+			if(befehle2[0] == "[refreshPLayers]"){
+				refreshPlayers(
+			}	
+		}	
+	};
 
+	let refreshPlayers = function(players){
+		
+		
+	};	
